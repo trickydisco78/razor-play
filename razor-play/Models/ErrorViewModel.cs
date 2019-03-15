b21c836adc03500ecfab4ee1319ed7f73c74af2e
trickydisco78 <trickydisco78@gmail.com>
2019-03-15 09:52:51 +0000

    initial commit

---
+++ razor-play/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace razor_play.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
